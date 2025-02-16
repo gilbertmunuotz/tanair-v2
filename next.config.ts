49b4e667b534af486e986b24272599bda9f9ff6c
gilbertmunuotz <officialgilbert24@gmail.com>
2025-02-16 22:09:01 +0300

    configured headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [{
+      source: "/api/auth/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "https://tanair-v2.vercel.app" },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+        { key: "Access-Control-Allow-Headers", value: "Authorization, Content-Type" },
+      ],
+    }]
+  }
 };
 
 export default nextConfig;
